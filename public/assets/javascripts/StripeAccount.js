c6ff9e619a4845cbe5f0c60b9d9a5ddef7ff0861
Tomasz Lemkowski <lemkowski@gmail.com>
2019-04-11 14:51:47 +0200

    account WIP

--- public/assets/javascripts/StripeAccount.js
+++ public/assets/javascripts/StripeAccount.js
@@ -16,17 +16,23 @@ async function handleForm(event) {
     console.log(result);
   }
 
-  // stripe.createToken('bank_account', {
-  //   country: 'US',
-  //   currency: 'usd',
-  //   routing_number: '110000000',
-  //   account_number: '000123456789',
-  //   account_holder_name: 'Jenny Rosen',
-  //   account_holder_type: 'individual',
-  // }).then(function(result) {
-  //   // Handle result.error or result.token
-  // });
-  const accountResult = await stripe.createToken('account', {
+  let stripePromises = [];
+
+  stripePromises.push(
+    stripe.createToken('bank_account', {
+      country: 'US',
+      currency: 'usd',
+      routing_number: '110000000',
+      account_number: '000123456789',
+      account_holder_name: 'Jenny Rosen',
+      account_holder_type: 'individual',
+    }).then(function(result) {
+      document.querySelector('[data-external-account]').value = result.token.id;
+    })
+  )
+  
+  stripePromises.push(
+    stripe.createToken('account', {
     business_type: 'company',
     company: {
       name: document.querySelector('.company [data-business-name]').value,
@@ -39,12 +45,49 @@ async function handleForm(event) {
       },
     },
     tos_shown_and_accepted: true,
+    }).then(function(result, person) {
+      document.querySelector('#account-token').value = result.token.id;
+    }));
+
+  const persons = document.querySelectorAll('.person')
+
+  for (i = 0; i < persons.length; ++i) {
+    let person = persons[i];
+    let stripePerson = { 
+      person: {
+        first_name: person.querySelector('[data-first-name]').value,
+        last_name: person.querySelector('[data-last-name]').value,
+        address: {
+          line1: person.querySelector('[data-address]').value,
+          city: person.querySelector('[data-city]').value,
+          state: person.querySelector('[data-state]').value,
+          postal_code: person.querySelector('[data-zip]').value,
+        }
+      }
+    }
+
+    let dobValue = person.querySelector('[data-date-of-birth]').value
+    if (dobValue.length != 0 ) {
+      let dob = dobValue.split('/');
+      stripePerson.dob = {}
+      if (dob[0] != undefined) stripePerson.dob.day = dob[0]
+      if (dob[1] != undefined) stripePerson.dob.month = dob[1]
+      if (dob[2] != undefined) stripePerson.dob.year = dob[2]
+    }
+
+    stripePromises.push(
+      stripe.createToken('person', stripePerson).then(
+        function(person, result) {
+          person.querySelector('[data-person-token]').value = result.token.id;
+        }.bind(null, person)
+      )
+    );
+
+  }
+
+  await Promise.all(stripePromises).then(function(result, person) {
+    myForm.submit();
   });
 
-
-  if (accountResult.token) {
-    document.querySelector('#account-token').value = accountResult.token.id;
-    myForm.submit();
-  }
 }
 
