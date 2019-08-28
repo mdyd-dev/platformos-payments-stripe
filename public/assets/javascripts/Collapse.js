d382cf21a4859612beb4874c7aec506db1973b27
Tomasz Lemkowski <lemkowski@gmail.com>
2019-08-28 10:26:09 +0200

    handle persons async

--- public/assets/javascripts/Collapse.js
+++ public/assets/javascripts/Collapse.js
@@ -1,27 +1,31 @@
 // Grab all the trigger elements on the page
-const triggers = Array.from(document.querySelectorAll('[data-toggle="collapse"]'));
+const triggers = Array.from(
+  document.querySelectorAll('[data-toggle="collapse"]'),
+);
 // Listen for click events, but only on our triggers
-window.addEventListener('click', (ev) => {
-  const elm = ev.target;
-  console.log("click");
-  if (triggers.includes(elm)) {
-    console.log("click 2");
-    if (elm.hasAttribute("href")) ev.preventDefault();
-    const selector = elm.getAttribute('data-target');
-    collapse(selector, 'toggle');
-  }
-}, false);
+window.addEventListener(
+  'click',
+  ev => {
+    const elm = ev.target;
+    if (triggers.includes(elm)) {
+      if (elm.hasAttribute('href')) ev.preventDefault();
+      const selector = elm.getAttribute('data-target');
+      collapse(selector, 'toggle');
+    }
+  },
+  false,
+);
 
 const fnmap = {
-  'toggle': 'toggle',
-    'show': 'add',
-    'hide': 'remove'
+  toggle: 'toggle',
+  show: 'add',
+  hide: 'remove',
 };
 const collapse = (selector, cmd) => {
-  console.log("COllapsing");
   const targets = Array.from(document.querySelectorAll(selector));
   targets.forEach(target => {
     target.classList[fnmap[cmd]]('collapse');
+    target.classList[fnmap[cmd]]('hidden');
     target.toggleAttribute('disabled');
   });
-}
+};
--- public/assets/javascripts/StripeAccount.js
+++ public/assets/javascripts/StripeAccount.js
@@ -1,3 +1,130 @@
+class StripePerson {
+  constructor(container, resolve) {
+    this.container = container;
+    this.resolve = resolve;
+    this.processInputData();
+
+    let frontFilePromise = this.setPersonDocument('front');
+    let backFilePromise = this.setPersonDocument('back');
+
+    return new Promise((resolve, reject) => {
+      Promise.all([frontFilePromise, backFilePromise]).then(
+        () => {
+          this.removeEmptyValues(this.personData);
+          console.log('this.personData', this.personData);
+          stripe.createToken('person', this.personData).then(
+            function(result) {
+              if (result.error) {
+                this.container.querySelector('.errors').textContent =
+                  result.error.message;
+                reject();
+              } else {
+                this.token = result.token.id;
+                this.savePerson().then(resolve());
+              }
+            }.bind(this),
+          );
+        },
+        error => {
+          console.log(error);
+        },
+      );
+    });
+  }
+
+  savePerson() {
+    let url = '/api/persons/' + this.personId;
+    let method = this.personId ? 'PUT' : 'POST';
+
+    const data = new FormData();
+    data.append('person_token', token);
+    data.append('account_id', this.accountId);
+
+    return fetch(url, {
+      method: method,
+      body: data,
+    });
+  }
+
+  setPersonDocument(side) {
+    let documentFile = this.container.querySelector(`[data-${side}-photo-id]`)
+      .files[0];
+    if (documentFile == undefined) return Promise.resolve();
+
+    return this.processFile(documentFile, side);
+  }
+
+  setDocumentToken(side, token) {
+    console.log('Setting document token', token);
+    if (this.personData.person.verification == undefined) {
+      this.personData.person.verification = {document: {}};
+    }
+    this.personData.person.verification.document[side] = token;
+  }
+
+  processFile(file, side) {
+    console.log('Processing file with', stripe._apiKey);
+    const data = new FormData();
+    data.append('file', file);
+    data.append('purpose', 'identity_document');
+    console.log('REQUEST', data);
+
+    return new Promise((resolve, reject) => {
+      fetch('https://uploads.stripe.com/v1/files', {
+        method: 'POST',
+        headers: {Authorization: `Bearer ${stripe._apiKey}`},
+        body: data,
+      })
+        .then(fileResponse => fileResponse.json())
+        .then(
+          function(json) {
+            this.setDocumentToken(side, json.id);
+            resolve();
+          }.bind(this),
+        );
+    });
+  }
+
+  removeEmptyValues(obj) {
+    for (var i in obj) {
+      if (obj[i] === null || obj[i].length == 0) {
+        delete obj[i];
+      } else if (typeof obj[i] === 'object') {
+        this.removeEmptyValues(obj[i]);
+      }
+    }
+  }
+
+  processInputData() {
+    this.personData = {
+      person: {
+        first_name: this.container.querySelector('[data-first-name]').value,
+        last_name: this.container.querySelector('[data-last-name]').value,
+        address: {
+          line1: this.container.querySelector('[data-address]').value,
+          city: this.container.querySelector('[data-city]').value,
+          state: this.container.querySelector('[data-state]').value,
+          postal_code: this.container.querySelector('[data-zip]').value,
+        },
+      },
+    };
+
+    let idNumberField = this.container.querySelector('[data-id-number]');
+    if (idNumberField) {
+      this.personData.person.id_number = idNumberField.value;
+    }
+
+    let dobValue = this.container.querySelector('[data-date-of-birth]').value;
+    if (dobValue.length != 0) {
+      let dob = dobValue.split('/');
+      this.personData.person.dob = {};
+      if (dob[0] != undefined) this.personData.person.dob.day = dob[0];
+      if (dob[1] != undefined) this.personData.person.dob.month = dob[1];
+      if (dob[2] != undefined) this.personData.person.dob.year = dob[2];
+    }
+  }
+}
+
 // Assumes you've already included Stripe.js!
 const stripe = Stripe(stripe_pk);
 console.log(stripe_pk);
@@ -75,7 +202,7 @@ const processIndividual = () => {
   return stripe
     .createToken('account', {
       business_type: 'individual',
-      individual: {},
+      individual: {email: 'text@example.com'},
       tos_shown_and_accepted: true,
     })
     .then(
@@ -111,7 +238,6 @@ const processCompany = () => {
   if (tax_id.length != 0) {
     company.tax_id = tax_id;
   }
-  console.log('COMPANY', company);
   return stripe
     .createToken('account', {
       business_type: 'company',
@@ -124,115 +250,15 @@ const processCompany = () => {
 };
 
 const processPersons = () => {
-  return new Promise((resolve, reject) => {
-    const persons = document.querySelectorAll('.person');
-    if (persons.length == 0) return resolve();
+  const persons = document.querySelectorAll('.person');
+  if (persons.length == 0) return resolve();
+  let personPromises = [];
 
-    for (i = 0; i < persons.length; ++i) {
-      let person = persons[i];
-
-      let stripePerson = {
-        person: {
-          first_name: person.querySelector('[data-first-name]').value,
-          last_name: person.querySelector('[data-last-name]').value,
-          address: {
-            line1: person.querySelector('[data-address]').value,
-            city: person.querySelector('[data-city]').value,
-            state: person.querySelector('[data-state]').value,
-            postal_code: person.querySelector('[data-zip]').value,
-          },
-        },
-      };
-
-      let idNumberField = person.querySelector('[data-id-number]');
-      if (idNumberField) {
-        stripePerson.person.id_number = idNumberField.value;
-      }
-
-      let dobValue = person.querySelector('[data-date-of-birth]').value;
-      if (dobValue.length != 0) {
-        let dob = dobValue.split('/');
-        stripePerson.person.dob = {};
-        if (dob[0] != undefined) stripePerson.person.dob.day = dob[0];
-        if (dob[1] != undefined) stripePerson.person.dob.month = dob[1];
-        if (dob[2] != undefined) stripePerson.person.dob.year = dob[2];
-      }
-
-      let frontFilePromise = setPersonDocument(person, stripePerson, 'front');
-      let backFilePromise = setPersonDocument(person, stripePerson, 'back');
-
-      Promise.all([frontFilePromise, backFilePromise]).then(
-        () => {
-          removeEmptyValues(stripePerson);
-          console.log('stripePerson', stripePerson);
-          stripe.createToken('person', stripePerson).then(
-            function(person, result) {
-              if (result.error) {
-                console.log('THEN', result.error);
-                person.querySelector('.errors').textContent =
-                  result.error.message;
-                reject();
-              } else {
-                console.log('Person Token', result.token.id);
-                person.querySelector('[data-person-token]').value =
-                  result.token.id;
-                console.log('Resolving PERSONS PROMISE');
-                resolve();
-              }
-            }.bind(null, person),
-          );
-        },
-        error => {
-          console.log(error);
-        },
-      );
-    }
-  });
-};
-
-const removeEmptyValues = obj => {
-  for (var i in obj) {
-    if (obj[i] === null || obj[i].length == 0) {
-      delete obj[i];
-    } else if (typeof obj[i] === 'object') {
-      removeEmptyValues(obj[i]);
-    }
+  for (i = 0; i < persons.length; ++i) {
+    personPromises.push(new StripePerson(persons[i]));
   }
-};
 
-const setPersonDocument = (person, stripePerson, side) => {
-  let documentFile = person.querySelector(`[data-${side}-photo-id]`).files[0];
-  if (documentFile == undefined) return Promise.resolve();
-
-  return processFile(documentFile, stripePerson, side);
-};
-
-const setDocumentToken = (stripePerson, side, token) => {
-  if (stripePerson.person.verification == undefined) {
-    stripePerson.person.verification = {document: {}};
-  }
-  stripePerson.person.verification.document[side] = token;
-};
-
-const processFile = (file, stripePerson, side) => {
-  console.log('Processing file with', stripe._apiKey);
-  const data = new FormData();
-  data.append('file', file);
-  data.append('purpose', 'identity_document');
-  console.log('REQUEST', data);
-
-  return new Promise((resolve, reject) => {
-    fetch('https://uploads.stripe.com/v1/files', {
-      method: 'POST',
-      headers: {Authorization: `Bearer ${stripe._apiKey}`},
-      body: data,
-    })
-      .then(fileResponse => fileResponse.json())
-      .then(json => {
-        setDocumentToken(stripePerson, side, json.id);
-        resolve();
-      });
-  });
+  return Promise.all(personPromises);
 };
 
 async function handleForm(event) {
@@ -250,7 +276,7 @@ async function handleForm(event) {
     .then(
       function(result) {
         console.log('SUBMITTING');
-        myForm.submit();
+        // myForm.submit();
       },
       function(error) {
         console.log('error');
